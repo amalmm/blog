8039d9b1f5107ae55243e915417b36202ba6f7a1
amal <amalramachandran8@gmail.com>
2024-09-25 13:37:21 +0530

    fixed design

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BlogController extends Controller
 {
@@ -29,9 +30,18 @@ class BlogController extends Controller
      */
     public function store(Request $request)
     {
-        Blog::create([
-            'title' => 'new blog created'
+
+         // Use Validator to manually handle the validation
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
         ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        Blog::create($validator->validated());
 
          return response()->json( ['message'=>'created'] ,200);
     }
@@ -57,9 +67,23 @@ class BlogController extends Controller
      */
     public function update(Request $request, Blog $blog)
     {
-        $blog->update([
-            'title' => $request->input('title')
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            // Add other validation rules as needed
         ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // If validation passes, get the validated data
+        $validatedData = $validator->validated();
+
+        // Update the blog with the validated data
+        $blog->update($validatedData);
+
         return  response()->json($blog, 200);
 
     }
